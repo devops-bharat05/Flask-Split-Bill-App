d9c024e589adee54128dc04608f5e7b4f2416316
Bharat Bhushan <116905352+devops-bharat05@users.noreply.github.com>
2024-10-25 08:10:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -126,4 +126,5 @@ def calculate(group_id):
     return render_template('result.html', group=group, result=result, total_expense=total_expense)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
